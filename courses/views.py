f8d6e4702f69c380c2a34bbce61a98506b532dac
Natalitta <nataliialebedieva@gmail.com>
2023-11-04 22:26:08 +0000

    Add search functionality

--- courses/views.py
+++ courses/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.contrib import messages
+# from django.contrib.auth.decorators import login_required
+from django.db.models import Q
+from django.db.models.functions import Lower
 from .models import Course, Category
 
 
@@ -7,10 +11,49 @@ def all_courses(request):
     # A view to show all products, including sorting and search queries
 
     courses = Course.objects.all()
+    query = None
+    categories = None
+    sort = None
+    direction = None
+
+    if request.GET:
+        if 'sort' in request.GET:
+            sortkey = request.GET['sort']
+            sort = sortkey
+            if sortkey == 'name':
+                sortkey = 'lower_name'
+                products = courses.annotate(lower_name=Lower('name'))
+
+            if sortkey == 'category':
+                sortkey = 'category__name'
+
+            if 'direction' in request.GET:
+                direction = request.GET['direction']
+                if direction == 'desc':
+                    sortkey = f'-{sortkey}'
+            courses = courses.order_by(sortkey)
+
+        if 'category' in request.GET:
+            categories = request.GET['category'].split(',')
+            courses = courses.filter(category__name__in=categories)
+            categories = Category.objects.filter(name__in=categories)
+
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                messages.error(request, "You should enter some search criteria!")
+                return redirect(reverse('courses'))
+
+            queries = Q(name__icontains=query) | Q(description__icontains=query)
+            courses = courses.filter(queries)
+
+    current_sorting = f'{sort}_{direction}'
 
     context = {
         'courses': courses,
-
+        'search_term': query,
+        'current_categories': categories,
+        'current_sorting': current_sorting,
     }
 
     return render(request, 'courses/courses.html', context)
