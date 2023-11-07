528737fd0fa5fc9d7b47ce6e78c6e9c15301019b
Natalitta <nataliialebedieva@gmail.com>
2023-11-07 23:11:11 +0000

    Register models in admin

--- checkout/admin.py
+++ checkout/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
+from .models import Order, OrderItem
 
-# Register your models here.
+
+class OrderItemAdmin(admin.TabularInline):
+    model = OrderItem
+    readonly_fields = ('orderitem_total',)
+
+
+class OrderAdmin(admin.ModelAdmin):
+    inlines = (OrderItemAdmin,)
+
+    readonly_fields = ('order_number', 'date',
+                       'order_total', 'original_bag',
+                        'stripe_pid',)
+
+    fields = ('order_number', #'user_profile', 
+                'date', 'full_name', 'email', 'phone_number', 
+                'order_total', 'original_bag', 'stripe_pid',)
+
+    list_display = ('order_number', 'date', 'full_name',
+                    'order_total',)
+    
+    ordering = ('-date',)
+
+admin.site.register(Order, OrderAdmin)
+
