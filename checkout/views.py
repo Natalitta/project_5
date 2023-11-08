4fea6d9e00b3ab81c8a559dfe2f46e0a3521c959
Natalitta <nataliialebedieva@gmail.com>
2023-11-08 23:25:03 +0000

    Add checkout submit js and checkout_done template

--- checkout/views.py
+++ checkout/views.py
@@ -4,34 +4,126 @@ from django.contrib import messages
 from django.conf import settings
 
 from .forms import OrderForm
-# from .models import Order, OrderItem
-#from courses.models import Course
+from .models import Order, OrderItem
+from courses.models import Course
 #from profiles.forms import UserProfileForm
 #from profiles.models import UserProfile
-#from bag.contexts import bag_contents
+from bag.contexts import bag_contents
 
-#import stripe
+import stripe
 #import json
 
 
 # Create your views here.
 def checkout(request):
-    # stripe_public_key = settings.STRIPE_PUBLIC_KEY
-    # stripe_secret_key = settings.STRIPE_SECRET_KEY
+    stripe_public_key = settings.STRIPE_PUBLIC_KEY
+    stripe_secret_key = settings.STRIPE_SECRET_KEY
 
-    # if request.method == 'POST':
-    bag = request.session.get('bag', {})
-    if not bag:
-        messages.error(request, "Your bag is empty")
-        return redirect(reverse('courses'))
+    if request.method == 'POST':
+        bag = request.session.get('bag', {})
+        form_data = {
+            'full_name': request.POST['full_name'],
+            'email': request.POST['email'],
+            'phone_number': request.POST['phone_number'],
+        }
+        order_form = OrderForm(form_data)
+        if order_form.is_valid():
+            order = order_form.save(commit=False)
+            #pid = request.POST.get('client_secret').split('_secret')[0]
+            #order.stripe_pid = pid
+            #order.original_bag = json.dumps(bag)
+            #order.save()
+            for item_id, item_data in bag.items():
+                try:
+                    course = Course.objects.get(id=item_id)
+                    if isinstance(item_data, int):
+                        order_item = OrderItem(
+                            order=order,
+                            course=course,
+                            quantity=item_data,
+                        )
+                        order_item.save()
+                except Course.DoesNotExist:
+                    messages.error(request, (
+                        "One of the courses in your bag wasn't found in our database. "
+                        "Please contact us for assistance!")
+                    )
+                    order.delete()
+                    return redirect(reverse('view_bag'))
 
-    order_form = OrderForm()
+            request.session['save_info'] = 'save-info' in request.POST
+            return redirect(reverse('checkout_done', args=[order.order_number]))
+        else:
+            messages.error(request, 'There was an error with your form. \
+                Please double check your information.')
+    else:
+        bag = request.session.get('bag', {})
+        if not bag:
+            messages.error(request, "There's nothing in your bag at the moment")
+            return redirect(reverse('courses'))
+
+        current_bag = bag_contents(request)
+        total = current_bag['total']
+        stripe_total = round(total * 100)
+        stripe.api_key = stripe_secret_key
+        intent = stripe.PaymentIntent.create(
+            amount=stripe_total,
+            currency=settings.STRIPE_CURRENCY,
+        )
+        order_form = OrderForm()
+
+    if not stripe_public_key:
+        messages.warning(request, 'Stripe public key is missing. \
+            Did you set it in your environment?')
 
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
-        'stripe_public_key': 'pk_test_51O6Y7LCijT1vLOXG6B3Rei2Mq4cHppwUw1TRHoFJM5HmqRdsALiSQ5BPgd2TE0H3kUJeCeFzsgPAW6ZHOW7nczJS00vXQ9rqtw',
-        'client_secret': 'test client', #intent.client_secret,
+        'stripe_public_key': stripe_public_key,
+        'client_secret': intent.client_secret,
+    }
+
+    return render(request, template, context)
+
+
+def checkout_done(request, order_number):
+    
+    # Handle successful checkouts
+    
+    save_info = request.session.get('save_info')
+    order = get_object_or_404(Order, order_number=order_number)
+
+    """if request.user.is_authenticated:
+        profile = UserProfile.objects.get(user=request.user)
+        # Attach the user's profile to the order
+        order.user_profile = profile
+        order.save()
+
+        # Save the user's info
+        if save_info:
+            profile_data = {
+                'default_phone_number': order.phone_number,
+                'default_country': order.country,
+                'default_postcode': order.postcode,
+                'default_town_or_city': order.town_or_city,
+                'default_street_address1': order.street_address1,
+                'default_street_address2': order.street_address2,
+                'default_county': order.county,
+            }
+            user_profile_form = UserProfileForm(profile_data, instance=profile)
+            if user_profile_form.is_valid():
+                user_profile_form.save()
+"""
+    messages.success(request, f'Order successfully processed! \
+        Your order number is {order_number}. A confirmation \
+        email will be sent to {order.email}.')
+
+    if 'bag' in request.session:
+        del request.session['bag']
+
+    template = 'checkout/checkout_done.html'
+    context = {
+        'order': order,
     }
 
     return render(request, template, context)
--- school/settings.py
+++ school/settings.py
@@ -176,9 +176,16 @@ EMBED_VIDEO_BACKENDS = (
     'my_app.backends.CustomBackend',
 )
 
+# Stripe
+FREE_MC_THRESHOLD = 99
+STRIPE_CURRENCY = 'eur'
+STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
+STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
+STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-FREE_MC_THRESHOLD = 99
+
