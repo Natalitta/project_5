2c88ffb069cf849bc198f7a2e8f9d25da5f43100
Natalitta <nataliialebedieva@gmail.com>
2023-11-08 13:06:22 +0000

    Add checkout template and crispy forms

--- checkout/views.py
+++ checkout/views.py
@@ -1,3 +1,37 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse, get_object_or_404, HttpResponse
+#from django.views.decorators.http import require_POST
+from django.contrib import messages
+from django.conf import settings
+
+from .forms import OrderForm
+# from .models import Order, OrderItem
+#from courses.models import Course
+#from profiles.forms import UserProfileForm
+#from profiles.models import UserProfile
+#from bag.contexts import bag_contents
+
+import stripe
+import json
+
 
 # Create your views here.
+def checkout(request):
+    # stripe_public_key = settings.STRIPE_PUBLIC_KEY
+    # stripe_secret_key = settings.STRIPE_SECRET_KEY
+
+    # if request.method == 'POST':
+    bag = request.session.get('bag', {})
+    if not bag:
+        messages.error(request, "Your bag is empty")
+        return redirect(reverse('courses'))
+
+    order_form = OrderForm(form_data)
+
+    template = 'checkout/checkout.html'
+    context = {
+        'order_form': order_form,
+        # 'stripe_public_key': stripe_public_key,
+        # 'client_secret': intent.client_secret,
+    }
+
+    return render(request, template, context)
--- school/settings.py
+++ school/settings.py
@@ -46,6 +46,10 @@ INSTALLED_APPS = [
     'courses',
     'bag',
     'checkout',
+
+     # Other
+    'crispy_forms',
+    #'storages',
 ]
 
 MIDDLEWARE = [
@@ -59,6 +63,8 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'school.urls'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 TEMPLATES = [
     {
@@ -76,6 +82,10 @@ TEMPLATES = [
                 'django.contrib.messages.context_processors.messages',
                 'bag.contexts.bag_contents',
             ],
+            'builtins': [
+                'crispy_forms.templatetags.crispy_forms_tags',
+                'crispy_forms.templatetags.crispy_forms_field',
+            ]
         },
     },
 ]
