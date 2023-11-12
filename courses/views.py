7ccf5f76d59f82854dae36e85501859317c88148
Natalitta <nataliialebedieva@gmail.com>
2023-11-12 13:20:56 +0000

    Add manage courses functionality

--- courses/views.py
+++ courses/views.py
@@ -4,6 +4,7 @@ from django.contrib.auth.decorators import login_required
 from django.db.models import Q
 from django.db.models.functions import Lower
 from .models import Course, Category
+from .forms import CourseForm
 
 
 # Create your views here.
@@ -70,3 +71,70 @@ def course_detail(request, course_id):
     }
 
     return render(request, 'courses/course_detail.html', context)
+
+@login_required
+def add_course(request):
+    # Add a course to the list of school courses
+    if not request.user.is_superuser:
+        messages.error(request, 'Sorry, only school admins can do that.')
+        return redirect(reverse('home'))
+
+    if request.method == 'POST':
+        form = CourseForm(request.POST, request.FILES)
+        if form.is_valid():
+            course = form.save()
+            messages.success(request, 'The course is successfully added!')
+            return redirect(reverse('course_detail', args=[course.id]))
+        else:
+            messages.error(request, 'Adding failed. Please ensure the form is valid.')
+    else:
+        form = CourseForm()
+
+    template = 'course/add_course.html'
+    context = {
+        'form': form,
+    }
+
+    return render(request, template, context)
+
+
+@login_required
+def edit_course(request, course_id):
+    # Edit a course in the store
+    if not request.user.is_superuser:
+        messages.error(request, 'Sorry, only store owners can do that.')
+        return redirect(reverse('home'))
+
+    course = get_object_or_404(Course, pk=course_id)
+    if request.method == 'POST':
+        form = CourseForm(request.POST, request.FILES, instance=course)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'The course is successfully updated!')
+            return redirect(reverse('course_detail', args=[course.id]))
+        else:
+            messages.error(request, 'Failed to update course. Please ensure the form is valid.')
+    else:
+        form = CourseForm(instance=course)
+        messages.info(request, f'You are editing {course.name}')
+
+    template = 'courses/edit_course.html'
+    context = {
+        'form': form,
+        'course': course,
+    }
+
+    return render(request, template, context)
+
+
+@login_required
+def delete_course(request, course_id):
+    """ Delete a course from the store """
+    if not request.user.is_superuser:
+        messages.error(request, 'Sorry, only school admins can do that.')
+        return redirect(reverse('home'))
+        
+    course = get_object_or_404(Course, pk=course_id)
+    course.delete()
+    messages.success(request, 'Course deleted!')
+    return redirect(reverse('courses'))
