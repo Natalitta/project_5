5b52db4bb99dc4899abab382fa5295af680c678b
Natalitta <nataliialebedieva@gmail.com>
2023-11-12 14:21:57 +0000

    Deployment

--- school/settings.py
+++ school/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 import os
+import dj_database_url
+
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -26,7 +28,8 @@ SECRET_KEY = 'django-insecure-hnvkj+6o)k=d+2&bs3!aai+0j+8#flgo5d3xb=28r09!th$=%&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-natalitta-project5-8r8w0rt01po.ws-eu106.gitpod.io']
+ALLOWED_HOSTS = ['8000-natalitta-project5-8r8w0rt01po.ws-eu106.gitpod.io',
+                'https://art-school-b21a3c0ad1c4.herokuapp.com/']
 
 
 # Application definition
@@ -120,12 +123,17 @@ WSGI_APPLICATION = 'school.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 # Password validation
