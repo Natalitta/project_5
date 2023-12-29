02975d965a93a39a72667337756faaa35d466fe2
Natalitta <nataliialebedieva@gmail.com>
2023-12-29 23:02:47 +0000

    Add comments model and add it to admin site

--- courses/admin.py
+++ courses/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Course, Category
+from .models import Course, Category, Comment
 
 
+@admin.register(Course)
 class CourseAdmin(admin.ModelAdmin):
     list_display = (
         'sku',
@@ -14,11 +15,21 @@ class CourseAdmin(admin.ModelAdmin):
 
     ordering = ('sku',)
 
+
+@admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
     list_display = (
         'friendly_name',
         'name',
     )
 
-admin.site.register(Course, CourseAdmin)
-admin.site.register(Category, CategoryAdmin)
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('name', 'body', 'course', 'posted_on', 'approved')
+    list_filter = ('approved', 'posted_on')
+    search_fields = ('name', 'body')
+    actions = ['approve_comments']
+
+    def approve_comments(self, request, queryset):
+        queryset.update(approved=True)
--- profiles/urls.py
+++ profiles/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from . import views
-from .views import add_to_wishlist
+from .views import wishlist, add_to_wishlist
 
 
 urlpatterns = [
     path('', views.profile, name='profile'),
     path('order_history/<order_number>', views.order_history, name='order_history'),
-    path('add_to_wishlist/<int:course_id>/', add_to_wishlist, name='add_to_wishlist'),
+    #path('wishlist/', views.wishlist, name='wishlist'),
+    path('add_to_wishlist/<int:course_id>/', views.add_to_wishlist, name='add_to_wishlist'),
 ]
